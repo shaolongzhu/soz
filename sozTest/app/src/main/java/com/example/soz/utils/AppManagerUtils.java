057b87d6518aa38892735651e0cb98f594988f1c
soz <974610666@qq.com>
2016-07-11 14:29:36 +0800

    add lanch mode test

---
+++ sozTest/app/src/main/java/com/example/soz/utils/AppManagerUtils.java
@@ -0,0 +1,35 @@
+package com.example.soz.utils;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+/**
+ * app manager tools
+ * Created by zhushaolong on 6/28/16.
+ */
+public class AppManagerUtils {
+
+    public static void runApp(Context context, String paramString) {
+        PackageManager localPackageManager = context.getPackageManager();
+        try {
+            PackageInfo localPackageInfo = localPackageManager.getPackageInfo(paramString, 0);
+            Intent localIntent1 = new Intent("android.intent.action.MAIN", null);
+            localIntent1.setPackage(localPackageInfo.packageName);
+            ResolveInfo localResolveInfo = (ResolveInfo)localPackageManager.queryIntentActivities(localIntent1, 0).iterator().next();
+            if (localResolveInfo != null)
+            {
+                String str1 = localResolveInfo.activityInfo.packageName;
+                String str2 = localResolveInfo.activityInfo.name;
+                Intent localIntent2 = new Intent("android.intent.action.MAIN");
+                localIntent2.setComponent(new ComponentName(str1, str2));
+                context.startActivity(localIntent2);
+            }
+        } catch (PackageManager.NameNotFoundException localNameNotFoundException) {
+            localNameNotFoundException.printStackTrace();
+        }
+    }
+}

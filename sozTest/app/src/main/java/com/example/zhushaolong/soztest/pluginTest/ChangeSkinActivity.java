ebd2e83e35b0c3c2a44d304661f6c87e74bbc316
soz <sozshaolong@gmail.com>
2016-07-27 15:57:26 +0800

    add slide menu animation for change skin

--- sozTest/app/src/main/java/com/example/zhushaolong/soztest/pluginTest/ChangeSkinActivity.java
+++ sozTest/app/src/main/java/com/example/zhushaolong/soztest/pluginTest/ChangeSkinActivity.java
@@ -3,9 +3,12 @@ package com.example.zhushaolong.soztest.pluginTest;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.soz.changeSkin.SkinManager;
 import com.example.soz.changeSkin.activity.BaseSkinActivity;
@@ -16,6 +19,7 @@ import com.example.soz.recyclerView.utils.LayoutManagerType;
 import com.example.soz.recyclerView.utils.RecyclerViewManager;
 import com.example.soz.utils.FragmentUtils;
 import com.example.zhushaolong.soztest.R;
+import com.nineoldandroids.view.ViewHelper;
 
 import java.io.File;
 import java.lang.reflect.Method;
@@ -30,6 +34,8 @@ public class ChangeSkinActivity extends BaseSkinActivity {
     private Logger mLogger = new Logger("ChangeSkinActivity");
     String[] data = new String[]{"item1", "item2", "item3"};
     List<String> mData = Arrays.asList(data);
+    DrawerLayout mDrawerLayout;
+    FrameLayout mContent;
     RecyclerView mRecyclerView;
 
     @Override
@@ -39,12 +45,15 @@ public class ChangeSkinActivity extends BaseSkinActivity {
         this.setContentView(R.layout.act_plugin_skin);
         this.initView();
         this.initData();
+        this.initEvent();
     }
 
     /**
      * init view
      */
     private void initView() {
+        mDrawerLayout = (DrawerLayout) this.findViewById(R.id.drawer_container);
+        mContent = (FrameLayout) this.findViewById(R.id.drawer_content);
         mRecyclerView = (RecyclerView) this.findViewById(R.id.drawer_menu);
     }
 
@@ -56,6 +65,61 @@ public class ChangeSkinActivity extends BaseSkinActivity {
         mRecyclerView.setAdapter(adapter);
         RecyclerViewManager.setRecyclerViewLayoutManager(this, mRecyclerView, LayoutManagerType.LINEAR_LAYOUT_MANAGER);
         FragmentUtils.addFragment(this, R.id.drawer_content, new ChangeSkinFragment());
+    }
+
+    /**
+     * init event
+     */
+    private void initEvent() {
+        this.mDrawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                mLogger.i("slideOffset = " + slideOffset);
+                if ("LEFT".equals(drawerView.getTag())) {
+                    leftSlide(drawerView, slideOffset);
+                } else {
+                    rightSlide(drawerView, slideOffset);
+                }
+            }
+        });
+    }
+
+    /**
+     * left menu slide
+     * @param view
+     * @param slideOffset
+     */
+    private void leftSlide(View view, float slideOffset) {
+        float scale = 1 - slideOffset;
+        float rightScale = 0.8f + scale * 0.2f;
+        float leftScale = 1 - 0.3f * scale;
+        ViewHelper.setScaleX(view, leftScale);
+        ViewHelper.setScaleY(view, leftScale);
+        ViewHelper.setAlpha(view, 0.6f + 0.4f * (1 - scale));
+        ViewHelper.setTranslationX(mContent,
+                view.getMeasuredWidth() * (1 - scale));
+        ViewHelper.setPivotX(mContent, 0);
+        ViewHelper.setPivotY(mContent,
+                mContent.getMeasuredHeight() / 2);
+        ViewHelper.setScaleX(mContent, rightScale);
+        ViewHelper.setScaleY(mContent, rightScale);
+    }
+
+    /**
+     * right menu slide
+     * @param view
+     * @param slideOffset
+     */
+    private void rightSlide(View view, float slideOffset) {
+        float scale = 1 - slideOffset;
+        float rightScale = 0.8f + scale * 0.2f;
+        ViewHelper.setTranslationX(mContent,
+                -view.getMeasuredWidth() * slideOffset);
+        ViewHelper.setPivotX(mContent,mContent.getMeasuredWidth());
+        ViewHelper.setPivotY(mContent,
+                mContent.getMeasuredHeight() / 2);
+        ViewHelper.setScaleX(mContent, rightScale);
+        ViewHelper.setScaleY(mContent, rightScale);
     }
 
     @Override

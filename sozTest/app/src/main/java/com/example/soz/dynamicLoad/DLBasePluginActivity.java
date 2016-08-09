450e0a6de04996b4f2ccc23e7bfb5ab923f6815d
soz <sozshaolong@gmail.com>
2016-08-09 18:51:30 +0800

    add plugin fameword base activity

--- sozTest/app/src/main/java/com/example/soz/dynamicLoad/DLBasePluginActivity.java
+++ sozTest/app/src/main/java/com/example/soz/dynamicLoad/DLBasePluginActivity.java
@@ -1,5 +1,7 @@
 package com.example.soz.dynamicLoad;
 
+import android.os.Bundle;
+
 import com.example.soz.activity.BaseActivity;
 import com.example.soz.log.Logger;
 
@@ -7,6 +9,42 @@ import com.example.soz.log.Logger;
  * plugin base activity, all plugin activity should extend it.
  * Created by zhushaolong on 8/8/16.
  */
-public abstract class DLBasePluginActivity extends BaseActivity{
+public abstract class DLBasePluginActivity extends BaseActivity implements DLPlugin{
     Logger mLogger = new Logger("DLBasePluginActivity");
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        mLogger.i("onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        mLogger.i("onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        mLogger.i("onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mLogger.i("onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        mLogger.i("onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        mLogger.i("onDestroy");
+        super.onDestroy();
+    }
 }
